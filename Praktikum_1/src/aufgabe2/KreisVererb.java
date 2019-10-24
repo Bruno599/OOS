e6b9a9bdabdd348f5bca30a69886d7b993d13acc
bruno481995@gmail.com <bruno481995@gmail.com>
2019-10-24 11:07:56 +0200

    aufgabe 2 begonnen

---
+++ Praktikum_1/src/aufgabe2/KreisVererb.java
@@ -0,0 +1,16 @@
+package aufgabe2;
+
+public class KreisVererb extends Point {
+
+    int radius;
+
+    public KreisVererb(){
+        super();
+        this.radius = 0;
+    }
+
+    public KreisVererb(Point p, int r){
+        super.
+    }
+
+}
---
+++ Praktikum_1/src/aufgabe2/Point.java
@@ -0,0 +1,27 @@
+package aufgabe2;
+
+public class Point
+{
+    int x;
+    int y;
+
+    public Point(){this.x = 0; this.y = 0;};
+
+    public Point(Point p){this.x = p.x; this.y = p.y;};
+
+    public Point(int x, int y){this.x = x; this.y = y;}
+
+    public Point getLocation(){return this;}
+
+    public void setLocation(Point p){this.x = p.x; this.y = p.y;}
+
+    public void setLocation(int x, int y){this.x = x; this.y = y;}
+
+    public void move(int dx, int dy){this.x += dx; this.y += dy;}
+
+    public boolean equals(Point p){return (this.x == p.x && this.y == p.y);}
+
+    public String toString(){ return ("X: " + this.x + " Y: " + this.y); }
+
+
+}
